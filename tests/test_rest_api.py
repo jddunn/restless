c99e84ea5d403f035029df79327e8d2e132e19c6
Johnny Dunn <johnnyfived@protonmail.com>
2020-03-18 23:28:36 +0000

    add basic rest api test; update deps

---
+++ tests/test_rest_api.py
@@ -0,0 +1,22 @@
+import unittest
+import os
+
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+app = FastAPI()
+
+@app.get("/")
+async def read_main():
+    return {"msg": "Hello World"}
+
+class MainAPITests(unittest.TestCase):
+
+    def test_base_api_setup(self):
+        client = TestClient(app)
+        response = client.get("/")
+        assert response.status_code == 200
+        assert response.json() == {"msg": "Hello World"}
+
+if __name__ == '__main__':
+    unittest.main()
