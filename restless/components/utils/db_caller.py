4310b5c4786d2dd03c5d063a9b0c694efcf6888f
Johnny Dunn <johnnyfived@protonmail.com>
2020-03-19 06:13:40 +0000

    organize dockerfile; add db_caller component

---
+++ restless/components/utils/db_caller.py
@@ -0,0 +1,9 @@
+import pyspark
+
+class DB_Caller:
+    """
+    Wrapper for database calls. Private methods will be called by higher-level `Utils`. Currently only supports Spark.
+    """
+
+    def __init__(self):
+        pass
